fb9626d6881c61ff59570f265c929febf7c05fcb
Matheus Angelicio <matheus.angelicio00@gmail.com>
2023-04-16 21:11:43 -0300

    finalizar projeto

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:login_with_animation/constants.dart';
@@ -12,70 +14,181 @@ class AuthScreen extends StatefulWidget {
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
-class _AuthScreenState extends State<AuthScreen> {
+class _AuthScreenState extends State<AuthScreen>
+    with SingleTickerProviderStateMixin {
   bool _isShowSignUp = false;
+
+  late AnimationController _animationController;
+  late Animation<double> _animationTextRotate;
+
+  void setuUpAnimation() {
+    _animationController =
+        AnimationController(vsync: this, duration: defaultDuration);
+
+    _animationTextRotate =
+        Tween<double>(begin: 0, end: 90).animate(_animationController);
+  }
+
+  void updateView() {
+    setState(() {
+      _isShowSignUp = !_isShowSignUp;
+    });
+    _isShowSignUp
+        ? _animationController.forward()
+        : _animationController.reverse();
+  }
+
+  @override
+  void initState() {
+    setuUpAnimation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          AnimatedPositioned(
-            duration: defaultDuration,
-            width: _size.width * 0.88,
-            height: _size.height,
-            left: _isShowSignUp ? -_size.width * 0.76 : 0,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _isShowSignUp = !_isShowSignUp;
-                });
-              },
-              child: Container(
-                color: login_bg,
-                child: LoginForm(),
-              ),
-            ),
-          ),
-          AnimatedPositioned(
-            duration: defaultDuration,
-            height: _size.height,
-            width: _size.width * 0.88,
-            left: _isShowSignUp ? _size.width * 0.12 : _size.width * 0.88,
-            child: Container(
-              color: signup_bg,
-              child: SignUpForm(),
-            ),
-          ),
-          AnimatedPositioned(
-            duration: defaultDuration,
-            left: 0,
-            top: _size.height * 0.1,
-            right: _isShowSignUp ? -_size.width * 0.06 : _size.width * 0.06,
-            child: CircleAvatar(
-              radius: 25,
-              backgroundColor: Colors.white60,
-              child: _isShowSignUp
-                  ? SvgPicture.asset(
-                      "assets/animation_logo.svg",
-                      color: signup_bg,
-                    )
-                  : SvgPicture.asset(
-                      "assets/animation_logo.svg",
-                      color: login_bg,
+      body: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, _) {
+            return Stack(
+              children: [
+                AnimatedPositioned(
+                  duration: defaultDuration,
+                  width: _size.width * 0.88,
+                  height: _size.height,
+                  left: _isShowSignUp ? -_size.width * 0.76 : 0,
+                  child: Container(
+                    color: login_bg,
+                    child: LoginForm(),
+                  ),
+                ),
+                AnimatedPositioned(
+                  duration: defaultDuration,
+                  height: _size.height,
+                  width: _size.width * 0.88,
+                  left: _isShowSignUp ? _size.width * 0.12 : _size.width * 0.88,
+                  child: Container(
+                    color: signup_bg,
+                    child: SignUpForm(),
+                  ),
+                ),
+                AnimatedPositioned(
+                  duration: defaultDuration,
+                  left: 0,
+                  top: _size.height * 0.1,
+                  right:
+                      _isShowSignUp ? -_size.width * 0.06 : _size.width * 0.06,
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white60,
+                    child: AnimatedSwitcher(
+                      duration: defaultDuration,
+                      child: _isShowSignUp
+                          ? SvgPicture.asset(
+                              "assets/animation_logo.svg",
+                              color: signup_bg,
+                            )
+                          : SvgPicture.asset(
+                              "assets/animation_logo.svg",
+                              color: login_bg,
+                            ),
                     ),
-            ),
-          ),
-          AnimatedPositioned(
-            duration: defaultDuration,
-            width: _size.width,
-            bottom: _size.height * 0.1,
-            right: _isShowSignUp ? -_size.width * 0.06 : _size.width * 0.06,
-            child: SocalButtns(),
-          )
-        ],
-      ),
+                  ),
+                ),
+                AnimatedPositioned(
+                  duration: defaultDuration,
+                  width: _size.width,
+                  bottom: _size.height * 0.1,
+                  right:
+                      _isShowSignUp ? -_size.width * 0.06 : _size.width * 0.06,
+                  child: SocalButtns(),
+                ),
+                AnimatedPositioned(
+                  duration: defaultDuration,
+                  bottom: _isShowSignUp
+                      ? _size.height / 2 - 80
+                      : _size.height * 0.3,
+                  left: _isShowSignUp ? 0 : _size.width * 0.44 - 80,
+                  child: AnimatedDefaultTextStyle(
+                    duration: defaultDuration,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: _isShowSignUp ? 20 : 32,
+                      fontWeight: FontWeight.bold,
+                      color: _isShowSignUp ? Colors.white : Colors.white70,
+                    ),
+                    child: Transform.rotate(
+                      angle: -_animationTextRotate.value * pi / 180,
+                      alignment: Alignment.topLeft,
+                      child: InkWell(
+                        onTap: () {
+                          if (_isShowSignUp) {
+                            updateView();
+                          } else {
+                            // Login
+                          }
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: defpaultPadding * 0.75),
+                          width: 160,
+                          child: Text(
+                            "Log In".toUpperCase(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Sign up text
+                AnimatedPositioned(
+                  duration: defaultDuration,
+                  bottom: !_isShowSignUp
+                      ? _size.height / 2 - 80
+                      : _size.height * 0.3,
+                  right: _isShowSignUp ? _size.width * 0.44 - 80 : 0,
+                  child: AnimatedDefaultTextStyle(
+                    duration: defaultDuration,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: !_isShowSignUp ? 20 : 32,
+                      fontWeight: FontWeight.bold,
+                      color: _isShowSignUp ? Colors.white : Colors.white70,
+                    ),
+                    child: Transform.rotate(
+                      angle: (90 - _animationTextRotate.value) * pi / 180,
+                      alignment: Alignment.topRight,
+                      child: InkWell(
+                        onTap: () {
+                          if (_isShowSignUp) {
+                            // Sign Up
+                          } else {
+                            updateView();
+                          }
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: defpaultPadding * 0.75),
+                          width: 160,
+                          child: Text(
+                            "Sign Up".toUpperCase(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
